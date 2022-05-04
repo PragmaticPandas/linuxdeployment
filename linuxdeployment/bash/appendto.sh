5f4f674f38bf32f4e06c3231e812b455b3cac356
PragmaticPandas <missionambition2018@gmail.com>
2022-05-04 07:35:58 -0400

    Delete linuxdeployment/bash directory

--- linuxdeployment/bash/appendto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#takes files from a directory that start with the phrase
-#"append to" and adds them to the end of the file with
-#the given name after the keywords in a different directory
-#also searches directories recursively
-
-#pseudocode:
-
-#
